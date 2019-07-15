73f28df5269f55f75cc7f56d423dbdafbae79b55
yanxin <327348556@qq.com>
2019-07-16 07:38:26 +0800

    test

--- chapter10/manager.js
+++ chapter10/manager.js
@@ -1,3 +1,4 @@
+/*
 window.onload = function() {
     var worker = new Worker("worker.js");
     
@@ -7,4 +8,20 @@ window.onload = function() {
         var message = "Worker says " + event.data;
         document.getElementById("output").innerHTML = message;
     };
+}*/
+
+window.onload = function() {
+    var numWorkers = 3;
+    var workers = [];
+    for (var i=0; i<numWorkers; i++) {
+        var worker = new Worker("worker.js");
+        worker.onmessage = function(event) {
+            alert(event.target + " says " + event.data);
+        };
+        
+        workers.push(worker);
+    }
+    for (var i=0; i<numWorkers; i++) {
+        workers[i].postMessage("ping");
+    }
 }
